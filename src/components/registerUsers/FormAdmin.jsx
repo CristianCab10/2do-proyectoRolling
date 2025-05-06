af4e476e5b20e38e5185870934c63985d33442ed
Daniel <danisuarze@gmail.com>
2025-05-06 16:33:13 -0300

    mejora de css formulario registro admin

--- src/components/registerUsers/FormAdmin.jsx
+++ src/components/registerUsers/FormAdmin.jsx
@@ -1,51 +1,50 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router';
-import Swal from 'sweetalert2';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
 
 const FormAdmin = () => {
-  const navigate = useNavigate()
-  const [errores, setErrores] = useState({})
+  const navigate = useNavigate();
+  const [errores, setErrores] = useState({});
   const [formAdmin, setFormAdmin] = useState({
     nombreApellido: "",
     email: "",
     contrasenia: "",
     repContrasenia: "",
-    terminosCondiciones: false
-  })
+    terminosCondiciones: false,
+  });
 
   const handleChangeFormAdmin = (ev) => {
-    const value = ev.target.type === "checkbox" ? ev.target.checked : ev.target.value
-    setFormAdmin({ ...formAdmin, [ev.target.name]: value })
-  }
+    const value =
+      ev.target.type === "checkbox" ? ev.target.checked : ev.target.value;
+    setFormAdmin({ ...formAdmin, [ev.target.name]: value });
+  };
 
   const handleClickFormAdmin = (ev) => {
-    ev.preventDefault()
-    const erroresReg = {}
-    const { nombreApellido, email, contrasenia, repContrasenia, terminosCondiciones } = formAdmin
+    ev.preventDefault();
+    const erroresReg = {};
+    const {
+      nombreApellido,
+      email,
+      contrasenia,
+      repContrasenia,
+      terminosCondiciones,
+    } = formAdmin;
 
-    if (!nombreApellido) {
-      erroresReg.nombreApellido = "Campo Nombre y Apellido está vacío."
-    }
+    if (!nombreApellido) erroresReg.nombreApellido = "Campo obligatorio.";
+    if (!email) erroresReg.email = "Campo obligatorio.";
+    if (!contrasenia) erroresReg.contrasenia = "Campo obligatorio.";
+    if (!repContrasenia) erroresReg.repContrasenia = "Campo obligatorio.";
+    if (!terminosCondiciones)
+      erroresReg.terminosCondiciones = "Debes aceptar los Términos.";
 
-    if (!email) {
-      erroresReg.email = "Campo Email está vacío.";
-    }
-
-    if (!contrasenia) {
-      erroresReg.contrasenia = "Campo Contraseña está vacío."
-    }
-
-    if (!repContrasenia) {
-      erroresReg.repContrasenia = "Campo Repetir Contraseña está vacío."
-    }
-
-    if (!terminosCondiciones) {
-      erroresReg.terminosCondiciones = "Debes aceptar los Términos y Condiciones."
-    }
-
-    setErrores(erroresReg)
+    setErrores(erroresReg);
 
     if (
       nombreApellido &&
@@ -55,21 +54,20 @@ const FormAdmin = () => {
       terminosCondiciones
     ) {
       if (contrasenia === repContrasenia) {
-        let adminsLs = []
-        //asegurarse de que adminsLs sea un array
+        let adminsLs = [];
         try {
-          const data = JSON.parse(localStorage.getItem("admins"))
-          if (Array.isArray(data)) {
-            adminsLs = data}}
-            catch (error) {
-              console.error("Error al leer admins del localStorage:", error)}
+          const data = JSON.parse(localStorage.getItem("admins"));
+          if (Array.isArray(data)) adminsLs = data;
+        } catch (error) {
+          console.error("Error al leer admins del localStorage:", error);
+        }
 
-        const emailExiste = adminsLs.some(admin => admin.email === email)
+        const emailExiste = adminsLs.some((admin) => admin.email === email);
         if (emailExiste) {
           Swal.fire({
             icon: "error",
             title: "Este correo ya está registrado",
-            text: "Por favor, utiliza un correo diferente."
+            text: "Por favor, utiliza un correo diferente.",
           });
           return;
         }
@@ -81,115 +79,140 @@ const FormAdmin = () => {
           contrasenia,
           terminosCondiciones,
           rol: "admin",
-          login: false
-        }
+          login: false,
+        };
 
-        Swal.fire({
-          title: "¡Registro exitoso!",
-          icon: "success"
-        });
-
-        adminsLs.push(nuevoAdmin)
-        localStorage.setItem("admins", JSON.stringify(adminsLs))
+        Swal.fire({ title: "¡Registro exitoso!", icon: "success" });
+        adminsLs.push(nuevoAdmin);
+        localStorage.setItem("admins", JSON.stringify(adminsLs));
 
         setFormAdmin({
           nombreApellido: "",
           email: "",
           contrasenia: "",
           repContrasenia: "",
-          terminosCondiciones: false
-        })
+          terminosCondiciones: false,
+        });
 
-        setTimeout(() => {
-          navigate("/loginAdmin");
-        }, 1500);
+        setTimeout(() => navigate("/loginAdmin"), 1500);
       } else {
-        Swal.fire({
-          icon: "error",
-          title: "Las contraseñas no coinciden",
-        })
+        Swal.fire({ icon: "error", title: "Las contraseñas no coinciden" });
       }
     }
-  }
+  };
 
   return (
-    <Form className='w-25'>
-      <Form.Group className="mb-3" controlId="idNombreApellido">
-        <Form.Label>Nombre y Apellido</Form.Label>
-        <Form.Control
-          type="text"
-          className={errores.nombreApellido ? "form-control is-invalid" : "form-control"}
-          value={formAdmin.nombreApellido}
-          name="nombreApellido"
-          onChange={handleChangeFormAdmin} maxLength={18}
-        />
-        {errores.nombreApellido && (
-          <p className="text-danger">{errores.nombreApellido}</p>
-        )}
-      </Form.Group>
+    <Container className="container-fluid mt-5 bg-secondary p-5">
+      <Row className="justify-content-center">
+        <Col md={6} lg={5}>
+          <Card className="animate__animated animate__fadeInDown shadow bg-secondary">
+            <Card.Body>
+              <div className="text-center mb-4">
+                <Image
+                  src="../images/logoBlancoFooter.png"
+                  roundedCircle
+                  alt="Logo"
+                  width={80}
+                  height={80}
+                />
+                <h4 className="mt-3 text-white text-center">
+                  Registro de Administrador
+                </h4>
+              </div>
 
-      <Form.Group className="mb-3" controlId="idEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          className={errores.email ? "form-control is-invalid" : "form-control"}
-          value={formAdmin.email}
-          name="email"
-          onChange={handleChangeFormAdmin} maxLength={26}
-        />
-        {errores.email && (
-          <p className="text-danger">{errores.email}</p>
-        )}
-      </Form.Group>
+              <Form>
+                <Form.Group className="mb-3" controlId="idNombreApellido">
+                  <Form.Label className="text-white">
+                    Nombre y Apellido
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="nombreApellido"
+                    value={formAdmin.nombreApellido}
+                    onChange={handleChangeFormAdmin}
+                    isInvalid={!!errores.nombreApellido}
+                    maxLength={18}
+                  />
+                  <Form.Text className="text-danger">
+                    {errores.nombreApellido}
+                  </Form.Text>
+                </Form.Group>
 
-      <Form.Group className="mb-3" controlId="idContrasenia">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control
-          type="password"
-          className={errores.contrasenia ? "form-control is-invalid" : "form-control"}
-          value={formAdmin.contrasenia}
-          name="contrasenia"
-          onChange={handleChangeFormAdmin} maxLength={10}
-        />
-        {errores.contrasenia && (
-          <p className="text-danger">{errores.contrasenia}</p>
-        )}
-      </Form.Group>
+                <Form.Group className="mb-3" controlId="idEmail">
+                  <Form.Label className="text-white">Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    value={formAdmin.email}
+                    onChange={handleChangeFormAdmin}
+                    isInvalid={!!errores.email}
+                    maxLength={26}
+                  />
+                  <Form.Text className="text-danger">{errores.email}</Form.Text>
+                </Form.Group>
 
-      <Form.Group className="mb-3" controlId="idRepContrasenia">
-        <Form.Label>Repetir Contraseña</Form.Label>
-        <Form.Control
-          type="password"
-          className={errores.repContrasenia ? "form-control is-invalid" : "form-control"}
-          value={formAdmin.repContrasenia}
-          name="repContrasenia"
-          onChange={handleChangeFormAdmin} maxLength={10}
-        />
-        {errores.repContrasenia && (
-          <p className="text-danger">{errores.repContrasenia}</p>
-        )}
-      </Form.Group>
+                <Form.Group className="mb-3" controlId="idContrasenia">
+                  <Form.Label className="text-white">Contraseña</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="contrasenia"
+                    value={formAdmin.contrasenia}
+                    onChange={handleChangeFormAdmin}
+                    isInvalid={!!errores.contrasenia}
+                    maxLength={10}
+                  />
+                  <Form.Text className="text-danger">
+                    {errores.contrasenia}
+                  </Form.Text>
+                </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check
-          type="checkbox"
-          label="Aceptar Términos y Condiciones"
-          name="terminosCondiciones"
-          checked={formAdmin.terminosCondiciones}
-          onChange={handleChangeFormAdmin}
-        />
-        {errores.terminosCondiciones && (
-          <p className="text-danger">{errores.terminosCondiciones}</p>
-        )}
-      </Form.Group>
+                <Form.Group className="mb-3" controlId="idRepContrasenia">
+                  <Form.Label className="text-white">
+                    Repetir Contraseña
+                  </Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="repContrasenia"
+                    value={formAdmin.repContrasenia}
+                    onChange={handleChangeFormAdmin}
+                    isInvalid={!!errores.repContrasenia}
+                    maxLength={10}
+                  />
+                  <Form.Text className="text-danger">
+                    {errores.repContrasenia}
+                  </Form.Text>
+                </Form.Group>
 
-      <div className="text-center">
-        <Button variant="primary" type="submit" onClick={handleClickFormAdmin}>
-          Registrarse
-        </Button>
-      </div>
-    </Form>
-  )
-}
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check
+                    type="checkbox"
+                    label="Aceptar Términos y Condiciones"
+                    name="terminosCondiciones"
+                    checked={formAdmin.terminosCondiciones}
+                    onChange={handleChangeFormAdmin}
+                    isInvalid={!!errores.terminosCondiciones}
+                  />
+                  <Form.Text className="text-white">
+                    {errores.terminosCondiciones}
+                  </Form.Text>
+                </Form.Group>
 
-export default FormAdmin
+                <div className="text-center">
+                  <Button
+                    variant="warning"
+                    type="submit"
+                    onClick={handleClickFormAdmin}
+                  >
+                    Registrarse
+                  </Button>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default FormAdmin;
